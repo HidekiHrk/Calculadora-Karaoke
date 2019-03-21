9acd3ecd09379e2a68b37fd43f604feef5d4989a
HidekiHrk <hidekihiroki123@gmail.com>
2019-03-21 19:08:24 -0300

    .

--- front_end/index.js
+++ front_end/index.js
@@ -6,3 +6,13 @@ const close_window = () => {
 const minimize = () => {
     this_window.minimize(true);
 }
+
+const add_window = (element) => {
+    nw.Window.open('front_end/add_note.html', {
+        x: this_window.x + 30, y: this_window.y + 30,
+        width: 353, height: 113,
+        resizable: false, frame:false, transparent:true },
+    win => {
+        
+    }
+)}
