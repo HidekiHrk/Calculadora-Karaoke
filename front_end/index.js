c30147c3ca66cf701dc1e81414931618b5c2ddb8
HidekiHrk <hidekihiroki123@gmail.com>
2019-03-22 18:33:26 -0300

    .

--- front_end/index.js
+++ front_end/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 var this_window = nw.Window.get(window);
 const close_window = () => {
     this_window.close()
@@ -10,6 +12,18 @@ const minimize = () => {
 const remove_parent = obj => {
     let point_to_remove = obj.parentNode;
     point_to_remove.parentNode.removeChild(point_to_remove);
+}
+
+const create_point = (point_list_block, nota) => {
+    let node = document.createElement('p');
+    node.appendChild(document.createTextNode(`${nota} `));
+    let node2 = document.createElement('button');
+    node2.appendChild(document.createTextNode('x'));
+    node2.addEventListener('click', () => { remove_parent(node2) });
+    node.appendChild(node2);
+    node.id = `point${(point_list_block.childNodes.length + 1).toString()}`;
+    node.className = 'point'
+    return node;
 }
 
 const add_window = (element) => {
@@ -25,14 +39,7 @@ const add_window = (element) => {
             if(point_list_block.getElementsByClassName('point').length < 3){
                 let doc = win.window.document;
                 let nota = doc.getElementById('nota_final').innerText;
-                let node = document.createElement('p');
-                node.appendChild(document.createTextNode(`${nota.trim()} `));
-                let node2 = document.createElement('button');
-                node2.appendChild(document.createTextNode('x'));
-                node2.addEventListener('click', () => { remove_parent(node2) });
-                node.appendChild(node2);
-                node.id = `point${(point_list_block.childNodes.length + 1).toString()}`;
-                node.className = 'point'
+                let node = create_point(point_list_block, nota.trim());
                 point_list_block.appendChild(node);
             }
             else{
@@ -54,7 +61,7 @@ const add_block = () => {
     let p_block = document.createElement('div');
     p_block.className = 'p_block'; p_block.id = `p${divs.length + 1}`;
     // Create input for participant name
-    let name_input = document.createElement('input');
+    let name_input = document.createElement('input'); name_input.className = 'name_input'
     name_input.type = 'text'; name_input.placeholder = 'Nome do Participante';
     p_block.appendChild(name_input); // append input to p_block
     // Create a non-important text node that refers to "Notas:"
@@ -74,5 +81,48 @@ const add_block = () => {
     p_rem.appendChild(document.createTextNode('x'));
     p_rem.addEventListener('click', () => { remove_parent(p_rem) });
     p_block.appendChild(p_rem); // and append the f*cking block REMOVER to the p_block :)
-    body_obj.insertBefore(p_block, body_obj.querySelector('#add_button'));
+    return body_obj.insertBefore(p_block, body_obj.querySelector('#add_button'));
+}
+
+const open_or_close_menu = (type) => {
+    let menu_obj = document.getElementById('menu_body');
+    let vs = menu_obj.style.visibility
+    switch(type){
+        case 'button':
+            menu_obj.style.visibility = vs == 'hidden' ? 'visible' : 'hidden';
+            break;
+        case 'menu':
+            menu_obj.style.visibility = 'hidden';
+            break;
+        case 'app_body':
+            menu_obj.style.visibility = 'hidden';
+            break;
+    }
+}
+
+const get_body_json = () => {
+    let bdy = document.getElementById('body');
+    let all_blocks = bdy.querySelectorAll('.p_block');
+    let json = {};
+    for(var x = 0; x < all_blocks.length; x++){
+        let block = all_blocks[x];
+        let name = block.querySelector('.name_input').value;
+        let note_list = Array.from(block.querySelectorAll('.point')).map(z => {
+            return parseInt(z.innerText.trim().replace(/[^0-9]/g, ''));
+        });
+        console.log(note_list)
+        json[x.toString()] = { name: name, notes: note_list }
+    }
+    console.log(json)
+    return json;
+}
+
+const save_file = () => {
+    let jsonstr = JSON.stringify(get_body_json(), null, 4 );
+    let file = new Blob([jsonstr], {type: 'application/karaoke'})
+    file = URL.createObjectURL(file);
+    let link = document.createElement('a');
+    link.href = file;
+    link.download = 'filename.karaoke'
+    link.click();
 }
